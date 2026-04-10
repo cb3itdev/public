e315bdc29bab5b0f2aa39225cca8d5f3c2226360
cb3itdev <48366977+cb3itdev@users.noreply.github.com>
2026-04-10 13:39:50 -0500

    UpdateTeams.cmd

--- UpdateTeams.cmd
+++ UpdateTeams.cmd
@@ -11,6 +11,7 @@ taskkill /im MS-Teams.exe /t /f
 echo.
 
 echo Downloading Teams
+if NOT exist C:\Temp mkdir C:\Temp
 powershell Invoke-WebRequest -Uri "https://statics.teams.cdn.office.net/production-windows-x86/lkg/MSTeamsSetup.exe" -OutFile C:\temp\MSTeamsSetup.exe
 timeout /t 2 /nobreak >nul
 echo.
