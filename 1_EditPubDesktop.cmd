4ca0ee71d471993e708f2201decd003058d43a52
cb3itdev <48366977+cb3itdev@users.noreply.github.com>
2026-08-25 09:07:34 -0500

    Update 1_EditPubDesktop.cmd

--- 1_EditPubDesktop.cmd
+++ 1_EditPubDesktop.cmd
@@ -1,4 +1,9 @@
 @echo off
+echo.
+::check for admin rights
+net session >nul 2>&1
+if not %errorlevel%==0 echo  This must be run as administrator & echo  Press any key to exit & pause > nul & exit
+
 echo Setting %public%\Desktop to editable by the users group
 icacls "%public%\Desktop" /grant Users:F /inheritance:e
 pause
